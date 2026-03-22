189a10e3610f4c1e1d3229cc6c540435d45bafef
Dorian Bourcet <d.bourcet@gmail.com>
2026-03-22 16:41:59 -0400

    Updates

---
+++ assets/proxyUrl-CSkbmEGr.js
@@ -0,0 +1,1 @@
+const t="https://diapason-proxy.dorianbourcet.workers.dev";function e(o){const r=new URL(o);return`${t}/${r.host}${r.pathname}${r.search}`}export{e as p};
--- assets/proxyUrl-DlcVGORs.js
+++
@@ -1,1 +0,0 @@
-const t="https://diapason-proxy.dorianbourcet.workers.dev";function n(r){const o=new URL(r);return`${t}/${o.host}${o.pathname}`}export{n as p};
